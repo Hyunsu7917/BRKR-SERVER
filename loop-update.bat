29cbb93d866ef35811a57241787a98c8a184378c
Hyunsu7917 <kc7917@naver.com>
2025-03-26 13:42:24 +0900

    part

--- loop-update.bat
+++ loop-update.bat
@@ -1,9 +1,14 @@
 @echo off
 chcp 65001 >nul
-echo 🔁 자동 동기화 시작 (3분마다 실행)
-
 :loop
-  echo [%date% %time%] ⏳ 동기화 시도 중...
+echo 💠 자동 동기화 시작 (3분마다 실행)
+echo [%DATE% %TIME%] ⏳ 동기화 시도 중...
+
+IF EXIST manual-mode.txt (
+  echo ⚠️ 수동 모드: 동기화 중단됨.
+) ELSE (
   node update-local-excel.js
-  timeout /t 180 >nul
+)
+
+timeout /t 10
 goto loop
